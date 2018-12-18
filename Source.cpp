3360334e69615d5ab6fe7d2f445c2cc71f71d2ba
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-18 14:42:59 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
-
-using namespace std;
 #include "Date.h"
 
-void main()
-{
+using namespace std;
+
+void main() {
 	Date::setDefaultDate();
 	Date d1 = Date::getDefaultDate();
 	d1.addYear(5);
-	d1.addDay(27+31+28);
+	d1.addDay(27 + 31 + 28);
 	cout << d1 << endl;
 
+	Date dob(21, 11 ,1986);
+	cout << "Age: " << dob.caclAge() << endl;
 	system("pause");
 }
