ed7d46c6ed919da2bf2587df06b40afa09507139
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-18 18:45:59 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,14 +3,17 @@
 
 using namespace std;
 
+// function main begins program execution
 void main() {
 	Date::setDefaultDate();
+	
 	Date d1 = Date::getDefaultDate();
 	d1.addYear(5);
 	d1.addDay(27 + 31 + 28);
 	cout << d1 << endl;
 
-	Date dob(21, 11 ,1986);
+	Date dob(21, 11, 1986);
 	cout << "Age: " << dob.caclAge() << endl;
+
 	system("pause");
-}
+} // end main
