8f4dce094e335df73a8279c5c1852d59c6c9379b
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-19 11:05:06 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,12 @@
 /**
- * @file Source.cpp
- *
- * @brief This code creates Date objects and execute some 
- * Date members for testing prupose.
- *
- * @author Saif Ullah Ijaz
- *
- */
+* @file Source.cpp
+*
+* @brief This code creates Date objects and execute some
+* Date members for testing prupose.
+*
+* @author Saif Ullah Ijaz
+*
+*/
 #include<iostream>
 #include "Date.h"
 
@@ -14,15 +14,15 @@ using namespace std;
 
 // function main begins program execution
 void main() {
-	Date::setDefaultDate();
-	
-	Date d1 = Date::getDefaultDate();
-	d1.addYear(5);
-	d1.addDay(27 + 31 + 28);
+	Date::sSetDefaultDate();
+
+	Date d1 = Date::sGetDefaultDate();
+	d1.AddYear(5);
+	d1.AddDay(27 + 31 + 28);
 	cout << d1 << endl;
 
 	Date dob(21, 11, 1986);
-	cout << "Age: " << dob.caclAge() << endl;
+	cout << "Age: " << dob.CaclAge() << endl;
 
 	system("pause");
 } // end main
